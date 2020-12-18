08a24342e7b52e96e4c56e16205d0b8402d3b9ed
Massimiliano <m.croci.ly@gmail.com>
2020-12-18 15:58:56 +0100

    working on the third

---
+++ snack-1/js/script.js
@@ -0,0 +1,30 @@
+// Primo snack
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while
+
+
+// I declare the variables
+var n;
+var sum = 0 // needed to be initialized in some way
+
+
+
+// ask user to key in 5 times an integer number
+// for (var i = 0; i < 5; i++) {
+//   n = parseInt(prompt('key in an integer number') );
+//   sum += n;
+//   //console.log(sum); // it means to sum by the cycle
+// }
+//
+// console.log(sum);
+
+
+// WHILE program version
+var i = 0;
+
+while (i <= 4) { // from 0 to 4 total 5 times
+  n = parseInt(prompt('key in an integer number') );
+  sum += n;
+  i++;
+}
+
+console.log(sum);
--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -2,38 +2,49 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array (edited)
 
 
-var empthyArray = [];
-var number;
 
-// for (var i = 0; i <= 5; i++) {
-//   number = parseInt(prompt('inserisci un numero') );
-//   if (number % 2 != 0) { // 1 % 2 fa 0 con resto 1, 3 % 2 f a 1 resto 1 5  % 2 fa 2 resto 1 (divisioni)
-//     empthyArray.push(number);
+// I declare an empthy array and the n variable
+var empthyArray = [];
+var n;
+
+
+// ask user 6 times to key in an integer number
+// for (var i = 0; i < 6; i++) {
+//   n = parseInt(prompt('key in an integer number') );
+//   console.log(n);
+//
+//   // push odd numbers in the array
+//   if (n % 2 != 0) {
+//     empthyArray.push(n);
 //   }
 // }
-
+//
 // console.log(empthyArray);
-// // The remainder operator (%) returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend.console.log(12 % 5);
-// // expected output: 2
-//
-// console.log(-12 % 5);
-// // expected output: -2
-//
-// console.log(4 % 2);
-// // expected output: 0
-//
-// console.log(-4 % 2);
-// // expected output: -0
 
+
+// WHILE program version
 var i = 0;
 
-
 while (i <= 5) {
-  number = parseInt(prompt('inserisci un numero') );
+  n = parseInt(prompt('key in an integer number') );
+  console.log(n);
   i++;
-  if (number % 2 != 0) {
-    empthyArray.push(number);
+  if (n % 2 != 0) { // 1%2 0 e resto 1, 2%2 1 e resto 0, 3%2 1 e resto 1, 4%2 2 e resto 0, 5%2 2 e resto 1.
+    empthyArray.push(n);
   }
 }
 
 console.log(empthyArray);
+
+// ******** The remainder operator (%) returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend.console.log(12 % 5);
+// // // expected output: 2
+// //
+// // console.log(-12 % 5);
+// // // expected output: -2
+// //
+// // console.log(4 % 2);
+// // // expected output: 0
+// //
+// // console.log(-4 % 2);
+// // // expected output: -0
+// ************
